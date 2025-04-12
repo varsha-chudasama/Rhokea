ec7dca89d7f896e6af0fc2edf471fb96d6dc3c89
niralikarmadhi <118166254+niralikarmadhi@users.noreply.github.com>
2025-04-12 12:42:25 +0530

    hero section

--- src/js/parts/plugins.js
+++ src/js/parts/plugins.js
@@ -6,7 +6,7 @@ export class Plugins {
     this.FindUsSlider();
   }
   FindUsSlider() {
-    $('.find-us-slider').slick({
+    $(".find-us-slider").slick({
       slidesToShow: 3,
       slidesToScroll: 1,
       infinite: false,
@@ -18,10 +18,52 @@ export class Plugins {
           breakpoint: 768,
           settings: {
             arrows: false,
-            slidesToShow: 3
-          }
-        }
-      ]
+            slidesToShow: 3,
+          },
+        },
+      ],
+    });
+  }
+  HeroSlider() {
+    $(".hero-slider").slick({
+      slidesToShow: 1,
+      slidesPerRow: 1,
+      fade: true,
+      arrows: false,
+      dots: true,
+    });
+
+    $(document).ready(function () {
+      function initializeSlider(sliderClass, paginationWrapperClass) {
+        var $slider = $(sliderClass);
+        var $paginationWrapper = $(paginationWrapperClass);
+        updatePagination(
+          0,
+          $slider.slick("getSlick").slideCount,
+          paginationWrapperClass
+        );
+        $slider.on("afterChange", function (event, slick, currentSlide) {
+          updatePagination(
+            currentSlide,
+            slick.slideCount,
+            paginationWrapperClass
+          );
+        });
+        var $slickDots = $slider.find(".slick-dots");
+        $paginationWrapper.append($slickDots);
+      }
+      function updatePagination(
+        currentSlide,
+        totalSlides,
+        paginationWrapperClass
+      ) {
+        var formattedCurrent = ("0" + (currentSlide + 1)).slice(-2);
+        var formattedTotal = ("0" + totalSlides).slice(-2);
+        $(paginationWrapperClass + " .pagination-display").text(
+          formattedCurrent + "/" + formattedTotal
+        );
+      }
+      initializeSlider(".hero-slider", ".hero-section .pagination-wrapper");
     });
   }
 }
