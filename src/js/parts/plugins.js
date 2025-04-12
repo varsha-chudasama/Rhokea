1d6aec7efb9bc30c8ae1b4c6f1c01539fdc7f37f
varsha <varsha@karmadhi.com>
2025-04-12 12:32:53 +0530

    category page

--- src/js/parts/plugins.js
+++ src/js/parts/plugins.js
@@ -3,7 +3,25 @@ import "slick-carousel";
 export class Plugins {
 
   init() {
-
+    this.FindUsSlider();
   }
-
+  FindUsSlider() {
+    $('.find-us-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: false,
+      arrows: true,
+      prevArrow: ".find-us-section .prev-arrow",
+      nextArrow: ".find-us-section .next-arrow",
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 3
+          }
+        }
+      ]
+    });
+  }
 }
