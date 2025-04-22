078564ee179e42137be28d8161b742642a5519e8
niralikarmadhi <118166254+niralikarmadhi@users.noreply.github.com>
2025-04-22 10:05:08 +0530

    changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20456,16 +20456,14 @@ var Plugins = /*#__PURE__*/function () {
     key: "init",
     value: function init() {
       this.HeroSlider();
-    }
-  }, {
-    key: "HeroSlide",
-    value: function HeroSlide() {}
       this.FindUsSlider();
+      this.ShopSlider();
+      this.ProductSlider();
     }
   }, {
     key: "FindUsSlider",
     value: function FindUsSlider() {
-      $('.find-us-slider').slick({
+      $(".find-us-slider").slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: false,
@@ -20479,6 +20477,102 @@ var Plugins = /*#__PURE__*/function () {
             slidesToShow: 3
           }
         }]
+      });
+    }
+  }, {
+    key: "HeroSlider",
+    value: function HeroSlider() {
+      $(".hero-slider").slick({
+        slidesToShow: 1,
+        slidesPerRow: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+      });
+      $(document).ready(function () {
+        function initializeSlider(sliderClass, paginationWrapperClass) {
+          var $slider = $(sliderClass);
+          var $paginationWrapper = $(paginationWrapperClass);
+          updatePagination(0, $slider.slick("getSlick").slideCount, paginationWrapperClass);
+          $slider.on("afterChange", function (event, slick, currentSlide) {
+            updatePagination(currentSlide, slick.slideCount, paginationWrapperClass);
+          });
+          var $slickDots = $slider.find(".slick-dots");
+          $paginationWrapper.append($slickDots);
+        }
+        function updatePagination(currentSlide, totalSlides, paginationWrapperClass) {
+          var formattedCurrent = ("0" + (currentSlide + 1)).slice(-2);
+          var formattedTotal = ("0" + totalSlides).slice(-2);
+          $(paginationWrapperClass + " .pagination-display").text(formattedCurrent + "/" + formattedTotal);
+        }
+        initializeSlider(".hero-slider", ".hero-section .pagination-wrapper");
+      });
+    }
+  }, {
+    key: "ShopSlider",
+    value: function ShopSlider() {
+      $(".shop-slider").slick({
+        slidesToShow: 6,
+        slidesPerRow: 1,
+        arrows: false,
+        dots: false,
+        responsive: [{
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 5
+          }
+        }, {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4
+          }
+        }, {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 475,
+          settings: {
+            slidesToShow: 2
+          }
+        }]
+      });
+    }
+  }, {
+    key: "ProductSlider",
+    value: function ProductSlider() {
+      $(".product-slider").slick({
+        slidesToShow: 4,
+        slidesPerRow: 1,
+        arrows: false,
+        dots: false,
+        responsive: [{
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 475,
+          settings: {
+            slidesToShow: 2
+          }
+        }]
+      });
+      $('.right-content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.left-slider'
+      });
+      $('.left-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.right-content',
+        vertical: true,
+        dots: false,
+        focusOnSelect: true
       });
     }
   }]);
@@ -20878,5 +20972,7 @@ jquery_default()(function () {
 });
 
 // ===========================================================================
-})(); 
+})();
+
+/******/ })()
 ;
